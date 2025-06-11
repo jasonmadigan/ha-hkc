02a19cac939167f82566726e37f5eda2041d2213
Crowbar Z <crowbarz@outlook.com>
2025-06-11 21:37:55 +0100

    Sort imports

--- custom_components/hkc_alarm/config_flow.py
+++ custom_components/hkc_alarm/config_flow.py
@@ -1,13 +1,10 @@
 """Config flow for HKC Alarm."""
 import logging
-from pyhkc.hkc_api import HKCAlarm
-from homeassistant import config_entries, exceptions
-from homeassistant.core import callback
 import voluptuous as vol
-
+from pyhkc.hkc_api import HKCAlarm
+from homeassistant import config_entries
+from homeassistant.core import callback
 from .const import DOMAIN, DEFAULT_UPDATE_INTERVAL, CONF_UPDATE_INTERVAL
-
-from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
